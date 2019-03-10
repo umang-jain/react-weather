b1f979316d9b7b414d08520f478ac237c1779607
umang-jain <umang.usict.042164@ipu.ac.in>
2019-03-10 23:19:24 +0530

    complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   // constructor(props){
@@ -21,14 +22,22 @@ class App extends React.Component {
     );
   }
 
+  renderContent(){
+    if(this.state.errorMessage && !this.state.lat){
+      return  <div>Error: {this.state.errorMessage}</div>;
+    }
+    if(!this.state.errorMessage && this.state.lat){
+      return  <SeasonDisplay lat={this.state.lat} />
+    }
+    return <Spinner message='Please accept Location request' />;
+  }
+
   render() {
-      if(this.state.errorMessage && !this.state.lat){
-        return  <div>Error: {this.state.errorMessage}</div>;
-      }
-      if(!this.state.errorMessage && this.state.lat){
-        return  <SeasonDisplay lat={this.state.lat} />
-      }
-      return <div>Loading!</div>;
+    return (
+      <div className='border red'>
+        {this.renderContent()}
+      </div>
+    );
   }
 }
 
